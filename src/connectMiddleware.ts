e0b94c562371baadfc350ccf8fb8c1073f15b961
Nafis Zaman <nafis.zaman-1@ou.edu>
2018-04-25 11:03:33 -0700

    Add initial connect middleware

---
+++ src/connectMiddleware.ts
@@ -0,0 +1,36 @@
+import { ConversationReference, Middleware, TurnContext, ActivityTypes, Activity } from 'botbuilder';
+
+export interface Connection {
+    userReferences: [ConversationReference, ConversationReference | null]
+}
+
+export interface ConnectionProvider {
+    findConnectedTo(ref: Partial<ConversationReference>): ConversationReference | null;
+}
+
+export class ConnectMiddleware implements Middleware {
+    private provider: ConnectionProvider;
+
+    public constructor(provider: ConnectionProvider) {
+        this.provider = provider;
+    }
+
+    public onTurn(context: TurnContext, next: () => Promise<void>): Promise<void> {
+        // Only handle message activities
+        if (context.activity.type !== ActivityTypes.Message || !context.activity.text) {
+            return next();
+        }
+
+        // If already connected, forward the message
+        const ref = TurnContext.getConversationReference(context.activity);
+        const connected = this.provider.findConnectedTo(ref);
+        if (connected !== null) {
+            return context.adapter.continueConversation(connected, (forwardContext => {
+                forwardContext.sendActivity(context.activity);
+            }));
+        }
+
+        // Otherwise, continue the pipeline
+        return next();
+    }
+}
