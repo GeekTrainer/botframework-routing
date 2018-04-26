4186a27c6c843f9ffac7b25d36fce4b0e25f61d8
joalmeid <joao.diogo.almeida@gmail.com>
2018-04-26 10:10:59 -0700

    import /w ;

--- src/Provider/ConnectionProvider.ts
+++ src/Provider/ConnectionProvider.ts
@@ -1,5 +1,5 @@
 import { ConversationReference } from 'botbuilder';
-import { Connection } from '../Models/Connection'
+import { Connection } from '../Models/Connection';
 
 export interface ConnectionProvider {
     getConnections(): Promise<Connection[]>;
