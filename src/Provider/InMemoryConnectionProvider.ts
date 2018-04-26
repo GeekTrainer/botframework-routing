7b9d32983273d268521a58703749fec5dd73d2eb
joalmeid <joao.diogo.almeida@gmail.com>
2018-04-26 13:33:42 -0700

    ts standard

--- src/Provider/InMemoryConnectionProvider.ts
+++ src/Provider/InMemoryConnectionProvider.ts
@@ -6,37 +6,37 @@ export class InMemoryConnectionProvider implements ConnectionProvider {
     private pendingConnections: PendingConnection[] = [];
     private establishedConnections: EstablishedConnection[] = [];
 
-    getPendingConnections(): Promise<PendingConnection[]> {
+    public getPendingConnections(): Promise<PendingConnection[]> {
         return Promise.resolve(this.pendingConnections);
     }
 
-    getEstablishedConnections(): Promise<EstablishedConnection[]> {
+    public getEstablishedConnections(): Promise<EstablishedConnection[]> {
         return Promise.resolve(this.establishedConnections);
     }
 
-    addPendingConnection(connection: PendingConnection): Promise<void> {
+    public addPendingConnection(connection: PendingConnection): Promise<void> {
         this.pendingConnections.push(connection);
         return Promise.resolve();
     }
 
-    addEstablishedConnection(connection: EstablishedConnection): Promise<void> {
+    public addEstablishedConnection(connection: EstablishedConnection): Promise<void> {
         this.establishedConnections.push(connection);
         return Promise.resolve();
     }
 
-    endConnection(connection: ConversationReference): Promise<void> {  
-        const pendingConnectionIndex = this.pendingConnections.findIndex(c=> 
-            (c.userReference.conversation == connection.conversation));     
-        const establishedConnectionIndex = this.establishedConnections.findIndex(c=> 
-            (c.userReferences[0].conversation == connection.conversation) || 
-            (c.userReferences[1].conversation == connection.conversation));
-        
-        if(pendingConnectionIndex !== -1){
+    public endConnection(connection: ConversationReference): Promise<void> {
+        const pendingConnectionIndex = this.pendingConnections.findIndex(c =>
+            (c.userReference.conversation === connection.conversation));
+        const establishedConnectionIndex = this.establishedConnections.findIndex(c =>
+            (c.userReferences[0].conversation === connection.conversation) ||
+            (c.userReferences[1].conversation === connection.conversation));
+
+        if (pendingConnectionIndex !== -1) {
             this.pendingConnections.splice(pendingConnectionIndex);
-        }        
-        if(establishedConnectionIndex !== -1){
+        }
+        if (establishedConnectionIndex !== -1) {
             this.establishedConnections.splice(establishedConnectionIndex);
-        }        
+        }
 
         return Promise.resolve();
     }
--- src/tests/helpers.ts
+++ src/tests/helpers.ts
@@ -3,12 +3,12 @@ import { ConversationReference, ChannelAccount, TestAdapter, TurnContext, Promis
 export class CustomTestAdapter extends TestAdapter {
     public continueConversation(reference: Partial<ConversationReference>, logic: (revocableContext: TurnContext) => Promiseable<void>): Promise<void> {
         // TestAdapter does this: return Promise.reject(new Error(`not implemented`));
-        // So tests fail as we call continueConversation in handoff-middleware        
-        var context = new TurnContext(this, reference);
-        return logic(context) as Promise<void>;  
+        // So tests fail as we call continueConversation in handoff-middleware
+        const context = new TurnContext(this, reference);
+        return logic(context) as Promise<void>;
     }
 }
 
 export const getConversationReference = (id: string, name: string) => {
     return TurnContext.getConversationReference( { from: { id, name } } as Activity ) as ConversationReference;
-}
+};
