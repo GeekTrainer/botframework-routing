cb9cd3c7af116ce5ac871e3664f66476259a1590
Nafis Zaman <nafis@ou.edu>
2018-05-04 10:12:54 -0700

    Prepare to be used externally
    export API via index.js, generate ts declarations

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export * from './ConnectHelper';
+export * from './Models/Connection';
+export * from './Provider/ConnectionProvider';
+export * from './Provider/InMemoryConnectionProvider';
+// export * from './Provider/TableStorageConnectionProvider';
